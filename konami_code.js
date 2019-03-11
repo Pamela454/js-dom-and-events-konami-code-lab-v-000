65dc722fc2764dae71fe9ba3ea3e0f04af32f4b6
Pamela Frieze <pfrieze@gmail.com>
2019-03-11 03:21:59 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init(e) {
   // Write your JavaScript code inside the init() function
-  const key = parseInt(e.which || e.detail || e.location); 
+  const key = parseInt(e.detail || e.location); 
   
     if (key === code[index]) {
       index++; 
