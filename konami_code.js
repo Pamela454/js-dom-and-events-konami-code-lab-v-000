35aa1e15827f25ef510e22390211d7610e022ac8
Pamela Frieze <pfrieze@gmail.com>
2019-03-11 03:52:45 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  function onKeyDownHandler(e) {
+  document.body.addEventListener('keydown',function (e) {
     const key = parseInt(e.which || e.detail || e.location); 
   
     if (key === code[index]) {
