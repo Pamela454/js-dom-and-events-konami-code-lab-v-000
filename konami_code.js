7c7d3f3f2026626181003da6b9fe408108859131
Pamela Frieze <pfrieze@gmail.com>
2019-03-11 03:30:25 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ let index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
   function onKeyDownHandler(e) {
-  const key = parseInt(e.which || e.detail || e.location); 
+    const key = parseInt(e.which || e.detail || e.location); 
   
     if (key === code[index]) {
       index++; 
